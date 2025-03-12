d53b09f469bebc6dfb580bf4bfb4c1be900181bf
jackmok33 <jackmok33@gmail.com>
2025-03-12 17:19:19 +0000

    added video section(link to be changed)

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,7 +96,22 @@ export default function Home() {
           </p>
         </div>
         <TeamSection />
-        {/* <HomepageFeatures /> */}
+        <div className= {styles.centeredText}>
+          <h2>Demo video</h2>
+          <div className={styles.videoContainer}>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/Bo3ZEbmgZ4A" // change the link to demo video
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+          </div>
+        </div>
+        <br></br>
+
       </main>
     </Layout>
   );
