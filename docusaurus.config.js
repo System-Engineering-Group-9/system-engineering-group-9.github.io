208031e9b3f51555be41fc173650f84d682faba3
jackmok33 <jackmok33@gmail.com>
2025-03-12 11:51:10 +0000

    added blank md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
